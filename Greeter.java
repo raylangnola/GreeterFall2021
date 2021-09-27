a9d0e14ca6bea0fe0ad25a2c8ddd3fb1b1e6ab52
Ray Lang <rlang@xula.edu>
2021-09-27 14:05:50 +0000

    fixed default

--- Greeter.java
+++ Greeter.java
@@ -2,7 +2,7 @@ public class Greeter {
     public static void main(String[] args) {               
         
         String nameToGreet = 
-            args.length > 0 ? args[0] : "Earthlings";       
+            args.length > 0 ? args[0] : "Residents of Earth";       
         
         System.out.println("Greetings " + nameToGreet);
     }
